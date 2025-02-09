4c634f6002c4f4bfa334ce7b6b03588ce497f318
Shiv Singh <shivsinghcse@gmail.com>
2025-02-10 01:25:25 +0530

    font-awesome cdn added pagination added api call Trending movie added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,37 @@
+// import axios from 'axios';
+import { useEffect, useState } from 'react';
+const Banner = () => {
+    const [bannerImage, setBannerImage] = useState('');
+    const [bannerTitle, setBannerTitle] = useState('');
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const resp = await fetch(
+            `https://api.themoviedb.org/3/trending/movie/day?api_key=59651246c3721f05bf8ed927e7cb6110&language=en-US&page=1`
+        );
+        const data = await resp.json();
+        setBannerImage(data.results[0].backdrop_path);
+        setBannerTitle(data.results[0].title);
+
+        // console.log(data.results);
+    };
+    return (
+        <>
+            <div
+                className="h-[30vh] w-[95%] mx-auto mt-5 bg-cover bg-center flex items-end"
+                style={{
+                    backgroundImage: `url(https://image.tmdb.org/t/p/original${bannerImage})`,
+                }}
+            >
+                <div className="text-white w-full text-center text-2xl font-semibold">
+                    {bannerTitle}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Banner;
---
+++ src/components/Movies.jsx
@@ -0,0 +1,90 @@
+import MovieCard from './MovieCard';
+import { useState, useEffect } from 'react';
+import Pagination from './Pagination';
+
+const Movies = () => {
+    const [pageNo, setPageNo] = useState(1);
+    const [trendingMovies, setTrendingMovies] = useState([]);
+    const [watchList, setWatchList] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, [pageNo]);
+
+    useEffect(() => {
+        // on page load, set the watchlist from LS
+        const watchListMovies = localStorage.getItem('movies');
+        if (watchListMovies) {
+            setWatchList(JSON.parse(watchListMovies));
+        }
+    }, []);
+
+    const fetchData = async () => {
+        const resp = await fetch(
+            `https://api.themoviedb.org/3/trending/movie/day?api_key=59651246c3721f05bf8ed927e7cb6110&language=en-US&page=${pageNo}`
+        );
+        const data = await resp.json();
+        setTrendingMovies(data.results);
+        // console.log(data.results);
+    };
+
+    const handlePrevious = () => {
+        if (pageNo > 1) {
+            setPageNo(pageNo - 1);
+        }
+    };
+    const handleNext = () => {
+        if (pageNo < trendingMovies.length) setPageNo(pageNo + 1);
+    };
+
+    console.log(watchList);
+    const addToWatchList = (movie) => {
+        console.log(movie);
+        const updatedMovies = [...watchList, movie];
+        setWatchList(updatedMovies);
+        localStorage.setItem('movies', JSON.stringify(updatedMovies));
+        // console.log(watchList);
+    };
+    const removeFromWatchList = (movie) => {
+        const filteredMovies = watchList.filter((watchListMovie) => {
+            return movie.id !== watchListMovie.id;
+        });
+        setWatchList(filteredMovies);
+        localStorage.setItem('movies', JSON.stringify(filteredMovies));
+    };
+
+    return (
+        <>
+            <h1 className="text-center my-12 text-3xl font-bold underline">
+                Trending Movies
+            </h1>
+            <div className="flex flex-wrap gap-10 justify-center my-12">
+                {trendingMovies.map((movie) => {
+                    return (
+                        <MovieCard
+                            movie={movie}
+                            key={movie.id}
+                            addToWatchList={addToWatchList}
+                            removeFromWatchList={removeFromWatchList}
+                            watchList={watchList}
+                            id={movie.id}
+                        />
+                    );
+                })}
+            </div>
+            <Pagination
+                handleNext={handleNext}
+                handlePrevious={handlePrevious}
+                pageNo={pageNo}
+            />
+        </>
+    );
+};
+
+export default Movies;
+
+// API KEY : 59651246c3721f05bf8ed927e7cb6110
+
+// url 'https://api.themoviedb.org/3/trending/movie/day?language=en-US'
+
+// IMG CDN : https://image.tmdb.org/t/p/original
