962f7ea4bb57939ea3a4bd109febe96492a3fa63
Shiv Singh <shivsinghcse@gmail.com>
2025-02-10 01:01:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1 className="flex justify-center items-center h-[100vh]">Hello, IMDB</h1>
+        </>
+    );
+}
+
+export default App;
